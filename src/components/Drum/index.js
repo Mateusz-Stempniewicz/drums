ecd92127c756a900b8792a538b87e88ff913a550
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-18 16:51:31 +0100

    add playing music

--- src/components/Drum/index.js
+++ src/components/Drum/index.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
-const DrumItem = styled('div')`
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-  border-radius: 8px;
-  border: 2px solid rgba(255, 255, 255, 0.3);
-  background: rgba(255, 255, 255, 0.2);
-  cursor: pointer;
-  transition: all 0.08s ease-out;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
+const DrumItem = styled('div')(
+  {
+    width: '100px',
+    height: '100px',
+    margin: '10px',
+    borderRadius: '8px',
+    border: '2px solid rgba(255, 255, 255, 0.3)',
+    background: 'rgba(255, 255, 255, 0.2)',
+    cursor: 'pointer',
+    transition: 'all 0.08s ease-out',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  props => ({ borderColor: props.isPlaying ? 'yellow' : 'rgba(255, 255, 255, 0.3)' })
+);
 
 const DrumLetter = styled('div')`
   font-size: 2rem;
@@ -33,11 +36,29 @@ const DrumName = styled('div')`
 
 const Drum = ({ letter, sound, soundName }) => {
   const [audio] = useState(new Audio(sound));
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const clickHandler = () => audio.play();
 
+  const keyPressHandler = e => {
+    if (letter === e.key.toUpperCase()) {
+      setIsPlaying(true);
+      audio.play();
+    } else {
+      setIsPlaying(false);
+    }
+  };
+
+  const keyReleaseHandler = () => setIsPlaying(false);
+
   return (
-    <DrumItem onClick={clickHandler}>
+    <DrumItem
+      onClick={clickHandler}
+      onKeyDown={keyPressHandler}
+      onKeyUp={keyReleaseHandler}
+      isPlaying={isPlaying}
+      tabIndex="0"
+    >
       <DrumLetter>{letter}</DrumLetter>
       <DrumName>{soundName}</DrumName>
     </DrumItem>

1370130437945fac630fa306f37710cc71bf90b6
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-11-23 15:42:37 +0100

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-// Global styling
-import './global-styles';
 
+import './global-styles';
 import DrumContainer from './containers/DrumsContainer';
 
 const sounds = require('../src/assets/sounds/*.wav');
-
-console.log(sounds);
 
 const App = () => <DrumContainer sounds={sounds} />;
 
